fa5ef013c528138f958288a04c9aea105a67d4bb
KayliePetersPXL <kaylie.peters@student.pxl.be>
2023-10-19 11:53:42 +0200

    AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioClip movementSound;
+    public AudioClip gameStartSound;
+    public AudioClip gameOverSound;
+    public AudioClip fullRownSound;
+    public AudioClip blockPlacedSound;
+    public AudioClip achievementUnlockedSound;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnEnable()
     {
-        
+        EventManager.OnMove += PlayMove;
+        EventManager.OnGameStart += PlayGameStart;
+        EventManager.OnGameOver += PlayGameOver;
+        EventManager.OnFullRow += PlayFullRow;
+        EventManager.OnBlockPlaced += PlayBlockPlaced;
+        EventManager.OnAchievementUnlocked += PlayAchievementUnlocked;
+
+    }
+
+    void OnDisable() 
+    {
+        EventManager.OnMove -= PlayMove;
+        EventManager.OnGameStart -= PlayGameStart;
+        EventManager.OnGameOver -= PlayGameOver;
+        EventManager.OnFullRow -= PlayFullRow;
+        EventManager.OnBlockPlaced -= PlayBlockPlaced;
+        EventManager.OnAchievementUnlocked -= PlayAchievementUnlocked;
+    }
+
+    private void PlayMove()
+    {
+        AudioSource.PlayClipAtPoint(movementSound, transform.position);
+    }
+    private void PlayGameStart()
+    {
+        AudioSource.PlayClipAtPoint(gameStartSound, transform.position);
+    }
+    private void PlayGameOver()
+    {
+        AudioSource.PlayClipAtPoint(gameOverSound, transform.position);
+    }
+    private void PlayFullRow()
+    {
+        AudioSource.PlayClipAtPoint(fullRownSound, transform.position);
+    }
+    private void PlayBlockPlaced()
+    {
+        AudioSource.PlayClipAtPoint(blockPlacedSound, transform.position);
+    }
+    private void PlayAchievementUnlocked()
+    {
+        AudioSource.PlayClipAtPoint(achievementUnlockedSound, transform.position);
     }
 }
