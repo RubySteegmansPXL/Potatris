ae292f887b925d2b3a5cc759b649718d948730f0
BenBaeyens <ben.baeyens@student.pxl.be>
2023-11-04 01:24:24 +0100

    Shape creator 2.0

--- Assets/Scripts/ShapeCreatorWindow.cs
+++ Assets/Scripts/ShapeCreatorWindow.cs
@@ -4,9 +4,19 @@ using System.Collections.Generic;
 
 public class ShapeCreatorWindow : EditorWindow
 {
-    private bool[,] grid = new bool[4, 4];
+    private bool[,] grid = new bool[5, 5];
     private bool isRotatable = true;
     private Vector2Int center = new Vector2Int(-1, -1);
+    private string shapeName = "NewShape";
+    private Color baseColor = Color.white; // Default color for the shape
+    private Color accentColor = Color.grey; // Default accent color
+    private Color lightColor = Color.clear; // Default light color, can be set to transparent
+    private bool useExistingColor = false;
+    private bool showColorSettings = true; // Variable to track the foldout state
+    private SpriteData existingSpriteData = null; // Field to hold the existing SpriteData
+
+    private string errorMessage = string.Empty; // Field to store the error message
+
 
     [MenuItem("Tetris/Shape Creator")]
     private static void ShowWindow()
@@ -20,10 +30,10 @@ public class ShapeCreatorWindow : EditorWindow
     {
         GUILayout.Label("Create New Tetris Shape", EditorStyles.boldLabel);
 
-        for (int y = 0; y < 4; y++)
+        for (int y = 0; y < 5; y++)
         {
             EditorGUILayout.BeginHorizontal();
-            for (int x = 0; x < 4; x++)
+            for (int x = 0; x < 5; x++)
             {
                 Color color = Color.black; // Default color for empty square
                 if (center.x == x && center.y == y)
@@ -34,78 +44,221 @@ public class ShapeCreatorWindow : EditorWindow
                 GUI.backgroundColor = color;
                 if (GUILayout.Button("", GUILayout.Width(50), GUILayout.Height(50)))
                 {
-                    // Set this block to white or make it the center if it's already white
-                    if (center.x == x && center.y == y)
-                    {
-                        // Clicking on the current center will clear it
-                        center = new Vector2Int(-1, -1);
-                    }
-                    else if (grid[x, y])
-                    {
-                        // This block is already white, set it to center
-                        if (center.x != -1)
-                        {
-                            // Clear the previous center if there was one
-                            grid[center.x, center.y] = false;
-                        }
-                        center = new Vector2Int(x, y);
-                    }
-                    else
-                    {
-                        // The block is black, set it to white
-                        grid[x, y] = true;
-                    }
-                    GUI.changed = true; // Mark the GUI as changed so it will repaint
+                    // Toggle the block state and center
+                    ToggleBlockState(x, y);
                 }
             }
             EditorGUILayout.EndHorizontal();
         }
 
+        // Reset background color for the rest of the UI
         GUI.backgroundColor = Color.white;
+
+        // Add fields for the shape name and colors
+        shapeName = EditorGUILayout.TextField("Shape Name", shapeName);
+        // Checkbox for using existing color
+        useExistingColor = EditorGUILayout.Toggle("Use Existing Color", useExistingColor);
+
+        // Dropdown for color settings
+        if (useExistingColor)
+        {
+            // Field to input an existing SpriteData
+            existingSpriteData = (SpriteData)EditorGUILayout.ObjectField("Color Settings", existingSpriteData, typeof(SpriteData), false);
+        }
+        else
+        {
+            // Foldout for color fields
+            showColorSettings = EditorGUILayout.Foldout(showColorSettings, "Color Settings");
+            if (showColorSettings)
+            {
+                EditorGUI.indentLevel++; // Increase the indent
+                baseColor = EditorGUILayout.ColorField("Base Color", baseColor);
+                accentColor = EditorGUILayout.ColorField("Accent Color", accentColor);
+                lightColor = EditorGUILayout.ColorField("Light Color", lightColor);
+                EditorGUI.indentLevel--; // Decrease the indent back to the previous state
+            }
+        }
+
         isRotatable = EditorGUILayout.Toggle("Is Rotatable", isRotatable);
+
 
         if (GUILayout.Button("Generate Shape"))
         {
             GenerateShape();
-            ResetGrid();
         }
+
+        // Display the error message if it's not empty
+        if (!string.IsNullOrEmpty(errorMessage))
+        {
+            EditorGUILayout.HelpBox(errorMessage, MessageType.Error);
+        }
+    }
+    private void ToggleBlockState(int x, int y)
+    {
+        // If clicking on the center, unselect it and remove the center designation
+        if (center.x == x && center.y == y)
+        {
+            grid[x, y] = false; // Unselect the square
+            center = new Vector2Int(-1, -1); // Remove the center
+        }
+        else if (grid[x, y])
+        {
+            // If there's no center, make this block the center
+            if (center.x == -1 && center.y == -1)
+            {
+                center = new Vector2Int(x, y);
+            }
+            else
+            {
+                // If there's already a center, and it's not this block, unselect this block
+                grid[x, y] = false;
+            }
+        }
+        else
+        {
+            // If the square is unselected, select it
+            grid[x, y] = true;
+        }
+
+        GUI.changed = true; // Mark the GUI as changed so it will repaint
     }
 
     private void GenerateShape()
     {
-        ShapeData shapeData = ScriptableObject.CreateInstance<ShapeData>();
-        shapeData.canRotate = isRotatable;
+        // Clear the previous error message
+        errorMessage = string.Empty;
 
-        List<ShapeSegmentData> segmentList = new List<ShapeSegmentData>(); // Use a list to handle an unknown number of segments
-
-        for (int x = 0; x < 4; x++)
+        // Validation checks
+        if (string.IsNullOrWhiteSpace(shapeName))
         {
-            for (int y = 0; y < 4; y++)
+            errorMessage = "Shape name cannot be empty.";
+            return;
+        }
+
+        if (center.x == -1 || center.y == -1)
+        {
+            errorMessage = "No center square selected.";
+            return;
+        }
+
+        bool hasSegment = false;
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
             {
                 if (grid[x, y])
                 {
-                    ShapeSegmentData segment = new ShapeSegmentData();
-                    segment.x = x - center.x;
-                    segment.y = y - center.y;
-                    segment.isCenter = (x == center.x && y == center.y);
-                    segmentList.Add(segment); // Add to list instead of array
+                    hasSegment = true;
+                    break;
+                }
+            }
+
+            if (hasSegment)
+            {
+                break;
+            }
+        }
+
+        if (!hasSegment)
+        {
+            errorMessage = "At least one square must be selected to create a shape.";
+            return;
+        }
+        SpriteData spriteDataToUse = null;
+
+        // Check if using existing color and if the provided SpriteData is not null
+        if (useExistingColor)
+        {
+            if (existingSpriteData == null)
+            {
+                errorMessage = "Please select an existing color.";
+                return;
+            }
+            spriteDataToUse = existingSpriteData;
+        }
+        else
+        {
+            // Check if the colors are valid (not completely transparent or default)
+            if (baseColor.a == 0 && accentColor.a == 0 && lightColor.a == 0)
+            {
+                errorMessage = "Please specify at least one color.";
+                return;
+            }
+
+            // Create new SpriteData with the specified colors
+            spriteDataToUse = ScriptableObject.CreateInstance<SpriteData>();
+            spriteDataToUse.baseColor = baseColor;
+            spriteDataToUse.accentColor = accentColor;
+            spriteDataToUse.lightColor = lightColor;
+
+            // Save the new SpriteData asset
+            string spriteDataAssetPath = $"Assets/TetrisShapes/{GetValidFileName(shapeName)}_SpriteData.asset";
+            AssetDatabase.CreateAsset(spriteDataToUse, spriteDataAssetPath);
+        }
+
+        // Proceed to create the shape data
+        ShapeData shapeData = ScriptableObject.CreateInstance<ShapeData>();
+        shapeData.canRotate = isRotatable;
+        List<ShapeSegmentData> segmentList = new List<ShapeSegmentData>();
+
+        for (int x = 0; x < 5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+                if (grid[x, y])
+                {
+                    ShapeSegmentData segment = new ShapeSegmentData
+                    {
+                        x = x - center.x,
+                        y = y - center.y,
+                        isCenter = (x == center.x && y == center.y)
+                    };
+                    segmentList.Add(segment);
                 }
             }
         }
 
-        shapeData.segments = segmentList.ToArray(); // Convert the list to an array now that we have all segments
+        shapeData.segments = segmentList.ToArray();
+        shapeData.spriteData = spriteDataToUse; // Assign the sprite data
 
-
-        AssetDatabase.CreateAsset(shapeData, $"Assets/TetrisShapes/NewShape_{GUID.Generate()}.asset");
+        // Save the shape asset
+        string shapeAssetPath = $"Assets/TetrisShapes/{GetValidFileName(shapeName)}.asset";
+        AssetDatabase.CreateAsset(shapeData, shapeAssetPath);
         AssetDatabase.SaveAssets();
+
         EditorUtility.FocusProjectWindow();
         Selection.activeObject = shapeData;
+
+        // Reset the grid after generating the shape
+        ResetGrid();
     }
+
 
     private void ResetGrid()
     {
-        grid = new bool[4, 4];
+        grid = new bool[5, 5];
         center = new Vector2Int(-1, -1);
-        Repaint();
+        shapeName = "NewShape"; // Reset the shape name to default
+        baseColor = Color.white; // Reset the base color to white
+        accentColor = Color.grey; // Reset the accent color to grey
+        lightColor = Color.clear; // Reset the light color to clear (transparent)
+        existingSpriteData = null; // Reset the existing sprite data reference
+        useExistingColor = false; // Reset the toggle for using existing color
+        showColorSettings = true; // Reset the foldout to be open
+
+        errorMessage = string.Empty; // Clear any error message
+
+        GUI.changed = true; // Mark the GUI as changed so it will repaint
+        Repaint(); // Request the GUI to repaint if necessary
+    }
+
+
+    // Helper method to get a valid file name
+    private string GetValidFileName(string name)
+    {
+        string invalidChars = System.Text.RegularExpressions.Regex.Escape(new string(System.IO.Path.GetInvalidFileNameChars()));
+        string invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+        return System.Text.RegularExpressions.Regex.Replace(name, invalidRegStr, "_");
     }
 }
