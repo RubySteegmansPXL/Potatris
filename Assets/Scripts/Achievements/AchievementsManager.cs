120468dcbef7ea695cf53e83a680ec2cf044cf30
BenBaeyens <ben.baeyens@student.pxl.be>
2024-02-08 18:38:34 +0100

    Loading screen warnings for loading (when no internet), achievement animation fix

--- Assets/Scripts/Achievements/AchievementsManager.cs
+++ Assets/Scripts/Achievements/AchievementsManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 
-public class AchievementsManager : MonoBehaviour {
-    
+public class AchievementsManager : MonoBehaviour
+{
+
     public static AchievementsManager instance;
     public List<Achievement> achievements;
     public GameObject achievementPrefab;
@@ -45,7 +46,7 @@ public class AchievementsManager : MonoBehaviour {
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         Debug.Log("ACHIEVEMENTS" + scene.buildIndex);
-        if (scene.buildIndex == 3) 
+        if (scene.buildIndex == 3)
         {
             Debug.Log("OnSceneLoaded Achievements");
             scrollViewContent = GameObject.FindGameObjectWithTag("ScrollViewContent").transform;
@@ -54,24 +55,24 @@ public class AchievementsManager : MonoBehaviour {
         }
     }
 
-    private void InstantiateAchievements() 
+    private void InstantiateAchievements()
     {
         for (int i = 1; i <= 10; i++)
+        {
+            Achievement achievement = new Achievement()
             {
-                Achievement achievement = new Achievement()
-                {
-                    id = $"ach_{i}",
-                    titleKey = $"ach_title_{i}",
-                    descKey = $"ach_desc_{i}",
-                    isUnlocked = false,
-                    progress = 0,
-                    goal = 100
-                };
+                id = $"ach_{i}",
+                titleKey = $"ach_title_{i}",
+                descKey = $"ach_desc_{i}",
+                isUnlocked = false,
+                progress = 0,
+                goal = 100
+            };
 
-                achievements.Add(achievement);
-            }
+            achievements.Add(achievement);
+        }
 
-            SaveAchievements();
+        SaveAchievements();
     }
 
     public void UpdateAchievement(string id, int progressToAdd)
@@ -90,7 +91,7 @@ public class AchievementsManager : MonoBehaviour {
             SaveAchievements();
         }
     }
-    
+
     public void PopulateScrollView()
     {
         // Clear the scroll view content
@@ -108,7 +109,7 @@ public class AchievementsManager : MonoBehaviour {
             Debug.Log(achievement.titleKey);
             GameObject achievementObject = Instantiate(achievementPrefab);
             achievementObject.transform.SetParent(scrollViewContent, false);
-            
+
             AchievementUI achievementUI = achievementObject.GetComponent<AchievementUI>();
             achievementUI.SetAchievement(achievement, false);
         }
@@ -124,6 +125,7 @@ public class AchievementsManager : MonoBehaviour {
         AchievementUI achievementUI = achievementPopUp.GetComponent<AchievementUI>();
         achievementUI.SetAchievement(achievement, true);
         achievementPopUp.transform.SetParent(popUpParent, false);
+        EventManager.AchievementUnlocked(new CustomEventArgs(gameObject));
         StartCoroutine(DestroyAchievement(achievementPopUp));
     }
 
@@ -165,19 +167,19 @@ public class AchievementsManager : MonoBehaviour {
         float counter = 0;
         Vector3 originalPosition = popUp.transform.position;
         popUp.transform.position = new Vector3(popUp.transform.position.x + 500, popUp.transform.position.y, popUp.transform.position.z);
-        while (counter < 10)
+        while (counter < 1)
         {
             counter += Time.deltaTime;
-            popUp.transform.position = Vector3.Lerp(popUp.transform.position, originalPosition, 10);
+            popUp.transform.position = Vector3.Lerp(popUp.transform.position, originalPosition, 0.1f);
             yield return null;
         }
         yield return new WaitForSeconds(3);
         //Move popUp to the right
         counter = 0;
-        while (counter < 0.7)
+        while (counter < 1)
         {
             counter += Time.deltaTime;
-            popUp.transform.position = Vector3.Lerp(popUp.transform.position, new Vector3(popUp.transform.position.x + 5, popUp.transform.position.y, popUp.transform.position.z), 2);
+            popUp.transform.position = Vector3.Lerp(popUp.transform.position, new Vector3(popUp.transform.position.x + 5, popUp.transform.position.y, popUp.transform.position.z), 0.9f);
             yield return null;
         }
         //Destroy popUp
